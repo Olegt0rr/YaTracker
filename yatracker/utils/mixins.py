bc1aac0b23e9608a40f7e6bf08b6a625bd860840
Oleg A <t0rr@mail.ru>
2023-07-25 12:24:40 +0300

    chore: refactor code style

--- yatracker/utils/mixins.py
+++ yatracker/utils/mixins.py
@@ -12,14 +12,15 @@ class ContextInstanceMixin:
     @classmethod
     def get_current(cls: type[T], no_error=True) -> T:
         if no_error:
-            return cls.__context_instance.get(None)  # type: ignore
-        return cls.__context_instance.get()  # type: ignore
+            return cls.__context_instance.get(None)
+        return cls.__context_instance.get()
 
     @classmethod
-    def set_current(cls: type[T], value: T):
+    def set_current(cls: type[T], value: T) -> None:
         if not isinstance(value, cls):
-            msg = f"Value should be instance of {cls.__name__!r}, not {type(value).__name__!r}"
-            raise TypeError(
-                msg,
+            msg = (
+                f"Value should be instance of {cls.__name__!r}, not "
+                f"{type(value).__name__!r}"
             )
-        cls.__context_instance.set(value)  # type: ignore
+            raise TypeError(msg)
+        cls.__context_instance.set(value)

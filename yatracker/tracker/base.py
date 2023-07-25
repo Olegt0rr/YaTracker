9e409fd5c29c4d1b938e44901e0e1539d704334c
Oleg A <t0rr@mail.ru>
2023-07-25 11:54:43 +0300

    feat: add async context manager

--- yatracker/tracker/base.py
+++ yatracker/tracker/base.py
@@ -53,3 +53,9 @@ class BaseTracker(ContextInstanceMixin):
     async def close(self):
         """Graceful closing."""
         await self._client.close()
+
+    async def __aenter__(self):
+        return self
+
+    async def __aexit__(self, exc_type, exc, tb):
+        await self.close()

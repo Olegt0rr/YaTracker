8ec5faeab6092cfb283f873171e1c2a7dcba0032
Oleg A <t0rr@mail.ru>
2023-08-25 20:50:21 +0300

    chore: update example

--- examples/basic.py
+++ examples/basic.py
@@ -1,36 +1,35 @@
-import asyncio as aio
+import asyncio
 
 from yatracker import YaTracker
 
 # CAUTION! Don't store credentials in your code!
-ORG_ID = 123456
-TOKEN = "AgAEA7qidDyAXAnwDvLnsA6Yu6WzFw"
-
-# define tracker
-tracker = YaTracker(ORG_ID, TOKEN)
+ORG_ID = ...
+TOKEN = ...
 
 
-async def foo():
-    # create issue
+async def main() -> None:
+    """Basic example.
+
+    This way you may create, get and edit an issue.
+    """
+    # define tracker (once)
+    tracker = YaTracker(ORG_ID, TOKEN)
+    
+    # create an issue
     issue = await tracker.create_issue("New Issue", "KEY")
     print(issue)
 
-    # get issue
+    # get the issue
     issue = await tracker.get_issue("KEY-1")
     print(issue)
 
-    # update issue
+    # update the issue
     issue = await tracker.edit_issue("KEY-1", description="Hello World")
     print(issue)
 
-
-# don't forget to close tracker on app shutdown
-async def on_shutdown():
+    # don't forget to close tracker on app shutdown (once)
     await tracker.close()
 
 
 if __name__ == "__main__":
-    loop = aio.get_event_loop()
-    loop.run_until_complete(foo())
-    loop.run_until_complete(on_shutdown())
-    loop.close()
+    asyncio.run(main)

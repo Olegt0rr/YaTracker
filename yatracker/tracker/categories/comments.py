9e4228774593169c1d3d55778e7d4b2256f09081
Дмитрий Анфимов <danfimov@yandex-team.ru>
2023-09-30 22:09:49 +0300

    feat: add ability to update and delete comments

--- yatracker/tracker/categories/comments.py
+++ yatracker/tracker/categories/comments.py
@@ -29,3 +29,35 @@ class Comments(BaseTracker):
             payload=payload,
         )
         return self._decode(Comment, data)
+
+    async def edit_comment(
+        self,
+        issue_id: str,
+        comment_id: str | int,
+        text: str,
+        attachment_ids: list[str] | None = None,
+    ) -> Comment:
+        """Update issue comment.
+
+        Source:
+        https://cloud.yandex.com/en/docs/tracker/concepts/issues/edit-comment
+        """
+        payload = self._prepare_payload(locals(), exclude=["issue_id", "comment_id"])
+        data = await self._client.request(
+            method="PATCH",
+            uri=f"/issues/{issue_id}/comments/{comment_id}",
+            payload=payload,
+        )
+        return self._decode(Comment, data)
+
+    async def delete_comment(self, issue_id: str, comment_id: str | int) -> bool:
+        """Delete issue comment.
+
+        Source:
+        https://cloud.yandex.com/en/docs/tracker/concepts/issues/delete-comment
+        """
+        await self._client.request(
+            method="DELETE",
+            uri=f"/issues/{issue_id}/comments/{comment_id}",
+        )
+        return True

2e2969ecf9777eeb89bae6cadee36198fc7a4aec
Oleg A <t0rr@mail.ru>
2023-10-30 17:50:54 +0300

    chore: don't repr YaTracker

--- yatracker/types/base.py
+++ yatracker/types/base.py
@@ -13,3 +13,14 @@ class Base(Printable, Struct, omit_defaults=True, rename="camel"):
     """Base structure class."""
 
     _tracker: Any = None  # this field will be filled after decoding
+
+    def __repr__(self) -> str:
+        """Represent the object without tech fields."""
+        fields = ", ".join(
+            [
+                f"{k}={getattr(self, k)!r}"
+                for k in self.__struct_fields__
+                if not k.startswith("_")
+            ],
+        )
+        return f"{self.__class__.__name__}({fields})"
